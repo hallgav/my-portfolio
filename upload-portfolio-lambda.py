50ff69f42b1deed0ec672771466e1950cdffe1ae
Gavin Hall <gavin.hall@versent.com.au>
2019-03-22 12:18:25 +1100

    fix to run as AWS lambda function

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,19 +1,28 @@
-import boto3
+import json
 import io
 import zipfile
 import mimetypes
+import boto3
 
-s3 = boto3.resource('s3')
 
-portfolio_bucket = s3.Bucket('portfolio.gavinhallphotography.com')
-build_bucket = s3.Bucket('portfoliobuild.gavinhallphotography.com')
+def lambda_handler(event, context):
 
-portfolio_zip = io.BytesIO()
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    s3 = boto3.resource('s3')
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm,
-                                        ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    portfolio_bucket = s3.Bucket('portfolio.gavinhallphotography.com')
+    build_bucket = s3.Bucket('portfoliobuild.gavinhallphotography.com')
+
+    portfolio_zip = io.BytesIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj, nm,
+                                            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
